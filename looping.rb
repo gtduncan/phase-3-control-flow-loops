351dd8a2ddf967fcf70cd7c75e8b3a36d2a36cd0
George Duncan <gtduncan@usc.edu>
2022-08-09 13:50:42 -0400

    commit tree a4a7bb711092f3892f79dca37df24205f7a88e90

--- looping.rb
+++ looping.rb
@@ -1,5 +1,10 @@
 def happy_new_year
-  # your code here
+  i=10
+  while i > 0
+    puts i
+    i-=1
+  end
+  puts 'Happy New Year!'
 end
 
 # No need to modify this code! Use this to implement the fizzbuzz_printer method.
@@ -16,9 +21,20 @@ def fizzbuzz(num)
 end
 
 def fizzbuzz_printer
-  # your code here
+  (1..100).each do |num|
+    puts fizzbuzz(num)
+  end
 end
 
 def reverse_string(str)
-  # your code here
+  i = str.length-1
+  puts i
+  newStr = ""
+  while i >= 0
+    newStr = newStr + str[i]
+    i-=1
+  end
+  newStr
 end
+
+reverse_string('hello')
